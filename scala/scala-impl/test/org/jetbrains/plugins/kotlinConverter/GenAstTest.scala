be825cd8142984a9ce66efa9c70d6f651ff8e5ca
Ilya Kirillov <darthorimar@gmail.com>
2018-07-04 13:37:07 +0300

    Defs

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/GenAstTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/GenAstTest.scala
@@ -1,36 +1,17 @@
 package org.jetbrains.plugins.kotlinConverter
 
-import java.io.File
-
-import com.intellij.openapi.command.impl.DummyProject
-import com.intellij.openapi.fileTypes.FileType
-import com.intellij.openapi.fileTypes.ex.FakeFileType
-import com.intellij.openapi.project.Project
-import com.intellij.openapi.vfs.{LocalFileSystem, VirtualFile}
-import com.intellij.psi.{PsiFile, PsiManager}
 import org.jetbrains.plugins.kotlinConverter.ast.Expr.Lit
-import org.jetbrains.plugins.kotlinConverter.ast.Stmt.{ClassDef, DefnDef, FileDef, MultiBlock}
-import org.jetbrains.plugins.kotlinConverter.ast.{AST, Expr, Type}
-import org.jetbrains.plugins.scala.ScalaFileType
-import org.jetbrains.plugins.scala.base.{ScalaLightCodeInsightFixtureTestAdapter, ScalaLightPlatformCodeInsightTestCaseAdapter}
-import org.jetbrains.plugins.scala.decompiler.ScClassFileViewProvider
-import org.jetbrains.plugins.scala.lang.formatting.settings.ScalaCodeStyleSettings
-import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-import org.jetbrains.plugins.scala.lang.psi.impl.{ScalaFileImpl, ScalaPsiElementFactory}
-import org.jetbrains.plugins.scala.project.ProjectContext
-import org.jetbrains.plugins.scala.util.TypeAnnotationSettings
-import org.junit.Test
-
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
+import org.jetbrains.plugins.kotlinConverter.ast.Stmt.{DefnDef, FileDef, MultiBlock}
+import org.jetbrains.plugins.kotlinConverter.ast.{AST, DefParam, Type}
+import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
 
 class GenAstTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
 
   def doTest(code: String, expectedAst: AST) = {
     configureFromFileTextAdapter("dummy.scala", code)
     val psiFile = getFileAdapter
-    println(ASTConverter.convert(psiFile))
-    assert(expectedAst == ASTConverter.convert(psiFile))
+    println(ASTConverter.gen(psiFile))
+    assert(expectedAst == ASTConverter.gen(psiFile))
   }
 
   def testClass = {
@@ -43,14 +24,12 @@ class GenAstTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
 
   def testDef = {
     val code =
-      """class A {
-        |  def foo = {
+      """def foo(x: Int) = {
         |    1
-        |  }
         |}
       """.stripMargin
-    val expected = FileDef("dummy.scala",Seq(),Seq(ClassDef("A",List(DefnDef("foo",Type("Int"),Seq(),MultiBlock(ArrayBuffer(Lit(Type("Int"),"1"))))))))
-
+    val expected =
+      FileDef("dummy.scala", Seq(),Seq(DefnDef("foo",Type("Int"),Seq(DefParam(Type("Int"),"x")),MultiBlock(Seq(Lit(Type("Int"),"1"))))))
     doTest(code, expected)
   }
 }
