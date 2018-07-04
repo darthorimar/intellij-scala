efc4445907906aaabd3fe10c80c757515581845f
Ilya Kirillov <darthorimar@gmail.com>
2018-07-04 17:55:45 +0300

    Basic kotlin printer

---
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
@@ -0,0 +1,29 @@
+package org.jetbrains.plugins.kotlinConverter
+
+import org.jetbrains.plugins.kotlinConverter.ast.Expr.Lit
+import org.jetbrains.plugins.kotlinConverter.ast.Stmt.{DefnDef, FileDef, MultiBlock}
+import org.jetbrains.plugins.kotlinConverter.ast.{AST, Expr, Type}
+import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+
+class ConvertTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
+
+  def doTest(code: String) = {
+    configureFromFileTextAdapter("dummy.scala", code)
+    val psiFile = getFileAdapter
+    println(Converter.convert(psiFile.asInstanceOf[ScalaFile]))
+//    assert(expectedAst == ASTConverter.gen(psiFile))
+  }
+
+ def test = {
+   doTest(
+     """
+       |class A {
+       |  def foo = {
+       |    Seq.empty.map(identity)
+       |  }
+       |}
+     """.stripMargin)
+ }
+}
+
