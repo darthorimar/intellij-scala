9a25a9893b17a425f19b5a05f26172e07d73ed16
Ilya Kirillov <darthorimar@gmail.com>
2018-07-09 14:04:22 +0300

    Def type fix

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
@@ -15,49 +15,11 @@ class ConvertTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
 
  def test = {
    doTest(
-     """package org.jetbrains.plugins.kotlinConverter
-       |
-       |import com.intellij.openapi.actionSystem.{AnAction, AnActionEvent, CommonDataKeys, LangDataKeys}
-       |import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-       |
-       |class ConvertScalaToKotlinAction extends AnAction {
-       |
-       |  override def update(e: AnActionEvent): Unit = {
-       |    val presentation = e.getPresentation
-       |
-       |    def enable(): Unit = {
-       |      presentation.setEnabled(true)
-       |      presentation.setVisible(true)
-       |    }
-       |
-       |    def disable(): Unit = {
-       |      presentation.setEnabled(false)
-       |      presentation.setVisible(false)
-       |    }
-       |
-       |    val elements =
-       |      Option(LangDataKeys.PSI_ELEMENT_ARRAY.getData(e.getDataContext))
-       |        .orElse {
-       |          Option(CommonDataKeys.PSI_FILE.getData(e.getDataContext)).map(Array(_))
-       |        }.getOrElse(Array.empty)
-       |    val toggle =
-       |      elements
-       |        .map(_.getContainingFile)
-       |        .exists ( x => x match {
-       |          case j: ScalaFile => j.getContainingDirectory.isWritable
-       |          case _ => false
-       |        })
-       |    if (toggle) enable()
-       |    else        disable()
-       |
-       |
-       |  }
-       |
-       |  override def actionPerformed(anActionEvent: AnActionEvent): Unit = {
-       |
-       |  }
+     """
+       |class A {
+       |  def foo(x: Int, y: Int)(z: String) =
+       |    foo(x,y)(z)
        |}
-       |
      """.stripMargin)
 
 
