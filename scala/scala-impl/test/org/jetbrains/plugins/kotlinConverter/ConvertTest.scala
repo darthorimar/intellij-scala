b724d869f452905d53c8efd637e65f1f7ce4f350
Ilya Kirillov <darthorimar@gmail.com>
2018-07-09 12:12:39 +0300

    Get rid of typeCont

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
@@ -2,7 +2,7 @@ package org.jetbrains.plugins.kotlinConverter
 
 import org.jetbrains.plugins.kotlinConverter.ast.Expr.Lit
 import org.jetbrains.plugins.kotlinConverter.ast.Stmt.{DefnDef, FileDef, MultiBlock}
-import org.jetbrains.plugins.kotlinConverter.ast.{AST, Expr, TypeCont}
+import org.jetbrains.plugins.kotlinConverter.ast.{AST, Expr}
 import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
 
