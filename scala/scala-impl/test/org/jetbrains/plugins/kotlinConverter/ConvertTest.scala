75d1cbac4742c9c5fde8b67a0e553fe23544133c
Ilya Kirillov <darthorimar@gmail.com>
2018-07-12 11:47:15 +0300

    Lambda fix

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/ConvertTest.scala
@@ -10,29 +10,29 @@ class ConvertTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
     configureFromFileTextAdapter("dummy.scala", scala)
     val psiFile = getFileAdapter
     val res = Converter.convert(psiFile.asInstanceOf[ScalaFile])
-    println(res)
     assert(kotlin.replaceAllLiterally(" ", "").replaceAllLiterally("\n", "") ==
       res.replaceAllLiterally(" ", "").replaceAllLiterally("\n", ""))
   }
 
-//  def testFuncCall() = {
-//    doTest("""def a = "ny".substring(1,2)""", """fun a(): String = "ny".substring(1,2)""")
-//  }
-//
-//  def testOptionConverters() = {
-//    doTest("def a = Some(1).map(x => x + 1).get",
-//      "fun a(): Int =1?.let { x -> x + 1}!!")
-//  }
+  def testFuncCall(): Unit = {
+    doTest("""def a = "ny".substring(1,2)""",
+      """fun a(): String = "ny".substring(1,2)""")
+  }
+
+  def testOptionConverters(): Unit = {
+    doTest("def a = Some(1).map(x => x + 1).get",
+      "fun a(): Int =1?.let { x -> x + 1}!!")
+  }
 
 
-    def test() = {
-      doTest(
-        """
-          |def a[T](x: Seq[T]) = x.isEmpty
-          |def b = a(Seq.empty[Int])
-        """.stripMargin,
-        "fun a(): Int =1?.let { x -> x + 1}!!")
-    }
+  def testUncarry(): Unit = {
+    doTest(
+      """def a(x: Int, b: String)(c: Char) = 1
+        |def b = a(1,"2")('3')
+      """.stripMargin,
+      """fun a(x: Int, b: String, c: Char): Int =1
+        |fun b(): Int =a(1, "2", '3')""".stripMargin)
+  }
 
 }
 
---
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/CurTest.scala
@@ -0,0 +1,23 @@
+package org.jetbrains.plugins.kotlinConverter
+
+import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
+import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
+
+class CurTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
+
+  def eval(scala: String): Unit = {
+    configureFromFileTextAdapter("dummy.scala", scala)
+    val psiFile = getFileAdapter
+    val res = Converter.convert(psiFile.asInstanceOf[ScalaFile])
+    println(res)
+  }
+
+  def test(): Unit = {
+    eval(
+      """def a(x:Int) = x
+        |def b = Seq(1,2).map(a)
+      """.stripMargin)
+  }
+
+}
+
