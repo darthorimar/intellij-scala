ee64f93317d62e693d5be116e8344bd26c2b7fc4
Ilya Kirillov <darthorimar@gmail.com>
2018-07-13 15:28:34 +0300

    Finish constructor matching

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/CurTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/CurTest.scala
@@ -13,14 +13,17 @@ class CurTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
   }
 
   def test(): Unit = {
+    //  case q if 1 == 1 => q + 1
+    //  case y if 1 != 1 => y
     eval(
       """
         |trait A
         |case class B(a: A, b: Int) extends A
         |case class C() extends A
         |
-        |def a(x: A) = x match {
-        |  case B(B(B(a,d), z), b: Int) => z
+        |def a(x: Any) = x match {
+        |  case B(B(q,z),e) if e > 2=> z
+        |  case q: Int if x > 5 => q
         | }
       """.stripMargin)
   }
