4dcc7845f0e5f6390892c5f59a455eae1e1e2f7f
Ilya Kirillov <darthorimar@gmail.com>
2018-07-18 14:19:28 +0300

    seq.apply to seq[i]

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/CurTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/CurTest.scala
@@ -1,14 +1,14 @@
 package org.jetbrains.plugins.kotlinConverter
 
+
 import org.jetbrains.plugins.scala.base.ScalaLightPlatformCodeInsightTestCaseAdapter
 import org.jetbrains.plugins.scala.lang.psi.api.ScalaFile
-
 class CurTest extends ScalaLightPlatformCodeInsightTestCaseAdapter {
 
   def eval(scala: String): Unit = {
     configureFromFileTextAdapter("dummy.scala", scala)
-    val psiFile = getFileAdapter
-    val res = Converter.convert(psiFile.asInstanceOf[ScalaFile])
+    val psiFile = getFileAdapter.asInstanceOf[ScalaFile]
+    val res = Converter.convert(psiFile)
     println(res)
   }
 
