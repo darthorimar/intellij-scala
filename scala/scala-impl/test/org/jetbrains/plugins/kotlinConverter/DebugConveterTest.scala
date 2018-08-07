3bbafd9d256a44d7fb50fdd1a71ac993ef38bc34
Ilya Kirillov <darthorimar@gmail.com>
2018-08-07 18:01:40 +0300

    Started call by name params

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
@@ -3,8 +3,6 @@ package org.jetbrains.plugins.kotlinConverter
 class DebugConveterTest extends ConverterTestBase {
   def testDebug(): Unit =
     doTest(
-      """class A
-        |class B extends A
-      """.stripMargin,"", true)
+      """def a(x: => Int) = x""".stripMargin,"", true)
 
 }
