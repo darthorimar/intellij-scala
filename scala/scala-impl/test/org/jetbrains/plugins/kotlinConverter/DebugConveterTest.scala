5b86f94ff7bb8f6b5227e28eb5348065742d72b6
Ilya Kirillov <darthorimar@gmail.com>
2018-08-07 14:36:13 +0300

    Fix bug with interface constructors

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
@@ -2,7 +2,9 @@ package org.jetbrains.plugins.kotlinConverter
 
 class DebugConveterTest extends ConverterTestBase {
   def testDebug(): Unit =
-    doExprTest(
-      """Seq(1).map(_+ 1)""".stripMargin,"", true)
+    doTest(
+      """class A
+        |class B extends A
+      """.stripMargin,"", true)
 
 }
