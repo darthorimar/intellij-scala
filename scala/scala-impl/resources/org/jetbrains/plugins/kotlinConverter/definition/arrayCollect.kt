b88864f96f011786bac4968fd54b63cbac4591eb
Ilya Kirillov <darthorimar@gmail.com>
2018-08-13 16:29:52 +0300

    Working with arrays

--- scala/scala-impl/resources/org/jetbrains/plugins/kotlinConverter/definition/arrayCollect.kt
+++ scala/scala-impl/resources/org/jetbrains/plugins/kotlinConverter/definition/arrayCollect.kt
@@ -1,9 +1,9 @@
-public fun <T, R : Any> List<T>.collect(f: (T) -> R): List<R> =
+public inline fun <T, reified R : Any> Array<T>.collect(f: (T) -> R): Array<R> =
         this.mapNotNull {
             try {
                 f(it)
             } catch (_: MatchError) {
                 null
             }
-        }
+        }.toTypedArray()
 
