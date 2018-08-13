596dabbc0a2ac912704dfc5697f797632d17f0d3
Ilya Kirillov <darthorimar@gmail.com>
2018-08-13 15:31:09 +0300

    Fix isFunction method

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
@@ -3,15 +3,7 @@ package org.jetbrains.plugins.kotlinConverter
 class DebugConveterTest extends ConverterTestBase {
   def testDebug(): Unit =
     doTest(
-      """trait I
-        |  case class A(i: I) extends I
-        |  case class B(x: Int) extends I
-        |  object O {
-        |    def unapply(arg: Int): Option[A] = Some(A(B(1)))
-        |  }
-        |  val q = Right(1) match {
-        |    case Right(O(A(B(x)))) => x
-        |  }
+      """def a =
         |""".stripMargin,"", doPrint = true)
 
 
