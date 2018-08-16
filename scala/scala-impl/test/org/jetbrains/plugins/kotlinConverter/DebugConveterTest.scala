6ecc06031afe08e07be2bedcd5e01dcb932ba4a9
Ilya Kirillov <darthorimar@gmail.com>
2018-08-16 14:06:49 +0300

    Fix imports from the same class

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
@@ -3,29 +3,7 @@ package org.jetbrains.plugins.kotlinConverter
 class DebugConveterTest extends ConverterTestBase {
   def testDebug(): Unit =
     doTest(
-      """class RefCollector  {
-        |  protected def action(ast: AST): Option[AST] =
-        |    ast match {
-        |      case ClassType(name) =>
-        |        val className = addImport(name)
-        |        Some(ClassType(className))
-        |      case JavaType(name) =>
-        |        val className = addImport(name)
-        |        Some(JavaType(className))
-        |      case r@RefExpr(_, None, name, _, _) =>
-        |        val className = addImport(name)
-        |        Some(copy(r).asInstanceOf[RefExpr].copy(referenceName = className))
-        |      case _ => None
-        |    }
-        |
-        |  private def addImport(name: String) = {
-        |    val importPath = name.stripSuffix("$")
-        |    if (name.contains(".") && !name.startsWith("scala."))
-        |      imports = imports + Import(importPath)
-        |    val className = name.split('.').last
-        |    className
-        |  }
-        |}
+      """val a = Seq(1,2)
         |
         |
         |""".stripMargin,"", doPrint = true)
