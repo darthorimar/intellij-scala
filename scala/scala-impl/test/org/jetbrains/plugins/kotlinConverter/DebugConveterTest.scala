e7c4a0a9f8bdc82370482e1ce0b219cb9ddab509
Ilya Kirillov <darthorimar@gmail.com>
2018-08-16 13:36:55 +0300

    Fix types

--- scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/kotlinConverter/DebugConveterTest.scala
@@ -3,7 +3,29 @@ package org.jetbrains.plugins.kotlinConverter
 class DebugConveterTest extends ConverterTestBase {
   def testDebug(): Unit =
     doTest(
-      """val a =  Seq.empty
+      """class RefCollector  {
+        |  protected def action(ast: AST): Option[AST] =
+        |    ast match {
+        |      case ClassType(name) =>
+        |        val className = addImport(name)
+        |        Some(ClassType(className))
+        |      case JavaType(name) =>
+        |        val className = addImport(name)
+        |        Some(JavaType(className))
+        |      case r@RefExpr(_, None, name, _, _) =>
+        |        val className = addImport(name)
+        |        Some(copy(r).asInstanceOf[RefExpr].copy(referenceName = className))
+        |      case _ => None
+        |    }
+        |
+        |  private def addImport(name: String) = {
+        |    val importPath = name.stripSuffix("$")
+        |    if (name.contains(".") && !name.startsWith("scala."))
+        |      imports = imports + Import(importPath)
+        |    val className = name.split('.').last
+        |    className
+        |  }
+        |}
         |
         |
         |""".stripMargin,"", doPrint = true)
